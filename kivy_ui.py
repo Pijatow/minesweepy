fed70d84b79684545b1982d66cbcc7742c46b200
pijatow <mahdimahanjalali@gmail.com>
2025-03-01 21:39:21 +0330

    feat: implement update_board method to re-render buttons and visualize changes ✨
    - Added update_board to refresh entire board after button clicks
    - Re-renders board after generation in build method
    - Visualizes cell updates

--- kivy_ui.py
+++ kivy_ui.py
@@ -23,6 +23,7 @@ class MinesweepyApp(App):
                 bomb_cell.cell = board[i][j]
                 grid.add_widget(bomb_cell)
                 self.buttons[i][j] = bomb_cell  # Store the button reference
+        self.update_board()
         return grid
 
     def on_click(self, instance, touch):
@@ -34,6 +35,35 @@ class MinesweepyApp(App):
                 cell.reveal()
             elif touch.button == "right":
                 cell.toggle_flagged()
+            self.update_board()
+
+    def update_board(self):
+        """Update the text and color of all buttons based on the board state."""
+        for i in range(y):
+            for j in range(x):
+                button = self.buttons[i][j]
+                cell = button.cell
+
+                # coloring the cells
+                if cell.state == "revealed":
+                    if cell.is_bomb:
+                        # Red for bomb
+                        rgba = [1, 0, 0, 1]
+                    elif cell.neighbor_bombs_count() == 0:
+                        # black for empty cells
+                        rgba = [0, 0, 0, 1]
+                    else:
+                        # Light gray for revealed
+                        rgba = [1, 1, 1, 0.3]
+                elif cell.state == "flagged":
+                    # Yellow for flagged
+                    rgba = [1, 1, 0, 1]
+                elif cell.state == "hidden":
+                    # Gray for hidden
+                    rgba = [1, 1, 1, 0.5]
+
+                button.background_color = rgba
+                button.text = str(cell)
 
 
 if __name__ == "__main__":
